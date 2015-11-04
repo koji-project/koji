26fb75920bc86663af996519132afd0f93e45f1d
Mike McLean <mikem@redhat.com>
2015-11-04 16:40:55 -0500

    set build.start_time in cg_import

--- hub/kojihub.py
+++ hub/kojihub.py
@@ -4721,6 +4721,8 @@ class CG_Importer(object):
             buildinfo = dslice(metadata['build'], ['name', 'version', 'release', 'extra'])
             # epoch is not in the metadata spec, but we allow it to be specified
             buildinfo['epoch'] = metadata['build'].get('epoch', None)
+            buildinfo['start_time'] = \
+                datetime.datetime.fromtimestamp(metadata['build']['start_time']).isoformat(' ')
             buildinfo['completion_time'] = \
                 datetime.datetime.fromtimestamp(metadata['build']['end_time']).isoformat(' ')
             build_id = new_build(buildinfo)
