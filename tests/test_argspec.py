4f97237a845eceb6bc58fe4ec63907d0e73d9322
Mike McLean <mikem@redhat.com>
2016-04-28 12:24:19 -0400

    more tests

--- tests/test_argspec.py
+++ tests/test_argspec.py
@@ -11,7 +11,7 @@ class ArgspecCase(unittest.TestCase):
     def test_apply_argspec(self):
         """Test the parse_NVR method"""
 
-        # Single param
+        # Start simple
         argspec = (['n'], None, None, None)
         self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
             argspec, (), None )
@@ -27,6 +27,42 @@ class ArgspecCase(unittest.TestCase):
             argspec, (1,), {'m':1} )
         self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
             argspec, (), {'m':1} )
+
+        # using *args
+        argspec = (['a', 'b'], 'args', None, None)
+        self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
+            argspec, (), None )
+        self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
+            argspec, (1,), None )
+        ret = koji.tasks.apply_argspec(argspec, (1,2), None)
+        self.assertEqual(ret, {'a':1, 'b':2, 'args': ()})
+        ret = koji.tasks.apply_argspec(argspec, (1,2,3), None)
+        self.assertEqual(ret, {'a':1, 'b':2, 'args': (3,)})
+        self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
+            argspec, (1,2), {'a':5} )
+        self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
+            argspec, (1,2), {'x':5} )
+        ret = koji.tasks.apply_argspec(argspec, (1,), {'b':2})
+        self.assertEqual(ret, {'a':1, 'b':2, 'args': ()})
+        self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
+            argspec, (1,), {'b':2, 'c':3} )
+
+        # using **kwargs
+        argspec = (['a', 'b'], None, 'kwargs', None)
+        self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
+            argspec, (), None )
+        self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
+            argspec, (), {} )
+        ret = koji.tasks.apply_argspec(argspec, (), {'a':1, 'b':2})
+        self.assertEqual(ret, {'a':1, 'b':2, 'kwargs':{}})
+        ret = koji.tasks.apply_argspec(argspec, (), {'a':1, 'b':2, 'c':3})
+        self.assertEqual(ret, {'a':1, 'b':2, 'kwargs':{'c':3}})
+        ret = koji.tasks.apply_argspec(argspec, (1,2), {'c':3})
+        self.assertEqual(ret, {'a':1, 'b':2, 'kwargs':{'c':3}})
+        ret = koji.tasks.apply_argspec(argspec, (1,), {'b': 2, 'c':3})
+        self.assertEqual(ret, {'a':1, 'b':2, 'kwargs':{'c':3}})
+        self.assertRaises(koji.ParameterError, koji.tasks.apply_argspec,
+            argspec, (1,2), {'b':2} )
 
 
 if __name__ == '__main__':
