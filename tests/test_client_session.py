feb619f4607039a4c83d320680378dd4e9ecd529
Mike McLean <mikem@redhat.com>
2016-11-18 17:37:35 -0500

    unit tests for new_session()

--- tests/test_client_session.py
+++ tests/test_client_session.py
@@ -30,6 +30,36 @@ class TestClientSession(unittest.TestCase):
         self.assertEqual(princ, 'host/koji.example.com@REALM')
         getfqdn.assert_not_called()
 
+    @mock.patch('koji.compatrequests.Session')
+    @mock.patch('requests.Session')
+    def test_new_session(self, rsession, compat_session):
+        opts = {'use_old_ssl': False}
+        ksession = koji.ClientSession('http://koji.example.com/kojihub', opts)
 
+        # init should have called new_session for us
 
+        rsession.assert_called_once()
+        compat_session.assert_not_called()
 
+    @mock.patch('koji.compatrequests.Session')
+    @mock.patch('requests.Session')
+    def test_new_session_old(self, rsession, compat_session):
+        opts = {'use_old_ssl': True}
+        ksession = koji.ClientSession('http://koji.example.com/kojihub', opts)
+
+        # init should have called new_session for us
+
+        rsession.assert_not_called()
+        compat_session.assert_called_once()
+
+    @mock.patch('koji.compatrequests.Session')
+    @mock.patch('requests.Session')
+    def test_new_session_close(self, rsession, compat_session):
+        opts = {'use_old_ssl': True}
+        ksession = koji.ClientSession('http://koji.example.com/kojihub', opts)
+        my_rsession = mock.MagicMock()
+        ksession.rsession = my_rsession
+
+        ksession.new_session()
+        my_rsession.close.assert_called()
+        self.assertNotEqual(ksession.rsession, my_rsession)
