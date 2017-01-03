5612b1a7095272df9469384fbfa076395f322b30
Tomas Kopecek <tkopecek@redhat.com>
2017-01-03 11:53:56 +0100

    Error message for missing certificates

--- koji/__init__.py
+++ koji/__init__.py
@@ -2174,6 +2174,10 @@ class ClientSession(object):
             raise AuthError('No certification provided')
         if serverca is None:
             raise AuthError('No server CA provided')
+        if not os.access(cert, os.R_OK):
+            raise AuthError("Certificate %s doesn't exist or is not accessible" % cert)
+        if not os.access(serverca, os.R_OK):
+            raise AuthError("Server CA %s doesn't exist or is not accessible" % serverca)
         # FIXME: ca is not useful here and therefore ignored, can be removed
         # when API is changed
 
