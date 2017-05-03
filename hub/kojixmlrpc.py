8567044a201da42382b403b6ccb36fcf387ab646
Tomas Kopecek <tkopecek@redhat.com>
2017-05-03 11:41:39 +0200

    python-modernize -f libmodernize.fixes.fix_classic_division

--- hub/kojixmlrpc.py
+++ hub/kojixmlrpc.py
@@ -19,6 +19,7 @@
 #       Mike McLean <mikem@redhat.com>
 
 from __future__ import absolute_import
+from __future__ import division
 from six.moves.configparser import RawConfigParser
 import datetime
 import inspect
@@ -689,7 +690,7 @@ def handler(req):
 
 def get_memory_usage():
     pagesize = resource.getpagesize()
-    statm = [pagesize*int(y)/1024 for y in "".join(open("/proc/self/statm").readlines()).strip().split()]
+    statm = [pagesize*int(y)//1024 for y in "".join(open("/proc/self/statm").readlines()).strip().split()]
     size, res, shr, text, lib, data, dirty = statm
     return res - shr
 
--- koji/daemon.py
+++ koji/daemon.py
@@ -21,6 +21,7 @@
 #       Mike Bonnet <mikeb@redhat.com>
 
 from __future__ import absolute_import
+from __future__ import division
 import koji
 import koji.tasks
 from koji.tasks import safe_rmtree
@@ -829,7 +830,7 @@ class TaskManager(object):
                 #accept this task)
                 bin_avail = avail.get(bin, [0])
                 self.logger.debug("available capacities for bin: %r" % bin_avail)
-                median = bin_avail[(len(bin_avail)-1)/2]
+                median = bin_avail[(len(bin_avail)-1)//2]
                 self.logger.debug("ours: %.2f, median: %.2f" % (our_avail, median))
                 if not self.checkRelAvail(bin_avail, our_avail):
                     #decline for now and give the upper half a chance
@@ -847,7 +848,7 @@ class TaskManager(object):
         Check our available capacity against the capacity of other hosts in this bin.
         Return True if we should take a task, False otherwise.
         """
-        median = bin_avail[(len(bin_avail)-1)/2]
+        median = bin_avail[(len(bin_avail)-1)//2]
         self.logger.debug("ours: %.2f, median: %.2f" % (avail, median))
         if avail >= median:
             return True
@@ -1040,7 +1041,7 @@ class TaskManager(object):
             raise IOError("No such directory: %s" % br_path)
         fs_stat = os.statvfs(br_path)
         available = fs_stat.f_bavail * fs_stat.f_bsize
-        availableMB = available / 1024 / 1024
+        availableMB = available // 1024**2
         self.logger.debug("disk space available in '%s': %i MB", br_path, availableMB)
         if availableMB < self.options.minspace:
             self.status = "Insufficient disk space: %i MB, %i MB required" % (availableMB, self.options.minspace)
