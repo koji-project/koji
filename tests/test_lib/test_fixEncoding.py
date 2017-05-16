efe59699c62d0bdaf4c69df248dd95ca95ee1447
Mike McLean <mikem@redhat.com>
2017-05-16 18:09:38 -0400

    fix some test constants

--- tests/test_lib/test_fixEncoding.py
+++ tests/test_lib/test_fixEncoding.py
@@ -5,6 +5,7 @@
 
 from __future__ import absolute_import
 import koji
+import six
 import unittest
 
 class FixEncodingTestCase(unittest.TestCase):
@@ -14,23 +15,23 @@ class FixEncodingTestCase(unittest.TestCase):
         # [ value, fixed ]
         ['', ''],
         [u'', ''],
-        [u'góðan daginn', 'g\xc3\xb3\xc3\xb0an daginn'],
-        [u'hej', 'hej'],
-        [u'zdravstvuite', 'zdravstvuite'],
-        [u'céad míle fáilte', 'c\xc3\xa9ad m\xc3\xadle f\xc3\xa1ilte'],
-        [u'dobrý den', 'dobr\xc3\xbd den'],
-        [u'hylô', 'hyl\xc3\xb4'],
-        [u'jó napot', 'j\xc3\xb3 napot'],
-        [u'tervehdys', 'tervehdys'],
-        [u'olá', 'ol\xc3\xa1'],
-        [u'grüezi', 'gr\xc3\xbcezi'],
-        [u'dobre dan', 'dobre dan'],
-        [u'hello', 'hello'],
-        [u'bună ziua', 'bun\xc4\x83 ziua'],
-        [u'こんにちは', '\xe3\x81\x93\xe3\x82\x93\xe3\x81\xab\xe3\x81\xa1\xe3\x81\xaf'],
-        [u'你好', '\xe4\xbd\xa0\xe5\xa5\xbd'],
-        [u'नमस्कार', '\xe0\xa4\xa8\xe0\xa4\xae\xe0\xa4\xb8\xe0\xa5\x8d\xe0\xa4\x95\xe0\xa4\xbe\xe0\xa4\xb0'],
-        [u'안녕하세요', '\xec\x95\x88\xeb\x85\x95\xed\x95\x98\xec\x84\xb8\xec\x9a\x94'],
+        [u'góðan daginn', six.b('g\xc3\xb3\xc3\xb0an daginn')],
+        [u'hej', six.b('hej')],
+        [u'zdravstvuite', six.b('zdravstvuite')],
+        [u'céad míle fáilte', six.b('c\xc3\xa9ad m\xc3\xadle f\xc3\xa1ilte')],
+        [u'dobrý den', six.b('dobr\xc3\xbd den')],
+        [u'hylô', six.b('hyl\xc3\xb4')],
+        [u'jó napot', six.b('j\xc3\xb3 napot')],
+        [u'tervehdys', six.b('tervehdys')],
+        [u'olá', six.b('ol\xc3\xa1')],
+        [u'grüezi', six.b('gr\xc3\xbcezi')],
+        [u'dobre dan', six.b('dobre dan')],
+        [u'hello', six.b('hello')],
+        [u'bună ziua', six.b('bun\xc4\x83 ziua')],
+        [u'こんにちは', six.b('\xe3\x81\x93\xe3\x82\x93\xe3\x81\xab\xe3\x81\xa1\xe3\x81\xaf')],
+        [u'你好', six.b('\xe4\xbd\xa0\xe5\xa5\xbd')],
+        [u'नमस्कार',  six.b('\xe0\xa4\xa8\xe0\xa4\xae\xe0\xa4\xb8\xe0\xa5\x8d\xe0\xa4\x95\xe0\xa4\xbe\xe0\xa4\xb0')],
+        [u'안녕하세요', six.b('\xec\x95\x88\xeb\x85\x95\xed\x95\x98\xec\x84\xb8\xec\x9a\x94')],
     ]
 
     def test_fixEncoding(self):
