12121dc19100d8b070b9fd36027c18a8f1762416
Yuming Zhu <yzhu@redhat.com>
2017-08-29 17:58:19 +0800

    add xjb and yaml type in archivetypes table to fix [issue#574](https://pagure.io/koji/issue/574)

--- docs/schema-upgrade-1.13-1.14.sql
+++ docs/schema-upgrade-1.13-1.14.sql
@@ -6,4 +6,8 @@ BEGIN;
 -- drop unused log_messages table
 DROP TABLE log_messages;
 
+-- add yaml and xjb file type in archivetypes
+insert into archivetypes (name, description, extensions) values ('yaml', 'YAML Ain''t Markup Language', 'yaml yml');
+insert into archivetypes (name, description, extensions) values ('xjb', 'JAXB(Java Architecture for XML Binding) Binding Customization File', 'xjb');
+
 COMMIT;
