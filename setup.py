022e4e157f896e4c23fbb415d63886436282e817
Tomas Kopecek <tkopecek@redhat.com>
2017-12-06 14:51:07 +0100

    update to 1.15 version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ def get_install_requires():
 
 setup(
     name="koji",
-    version="1.14.0",
+    version="1.15.0",
     description=("Koji is a system for building and tracking RPMS. The base"
                  " package contains shared libraries and the command-line"
                  " interface."),
