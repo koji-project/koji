442ec600a7291b79d26eef30431ddfc8e6bf6bd9
Franz Chih-Ping Hsieh <chsieh@redhat.com>
2017-12-13 20:57:12 +0000

    add unit test for taskinfo() in koji-web.

---
+++ tests/test_www/loadwebindex.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+import os
+import imp
+
+# We have to do this craziness because 'import koji' is ambiguous.  Is it the
+# koji module, or the koji cli module.  Jump through hoops accordingly.
+# https://stackoverflow.com/questions/67631/how-to-import-a-module-given-the-full-path
+INDEX_FILENAME = os.path.dirname(__file__) + "/../../www/kojiweb/index.py"
+
+webidx = imp.load_source('index_fake', INDEX_FILENAME)
