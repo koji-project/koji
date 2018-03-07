046897159416786c323efa1d89716555e42c8c65
Tomas Kopecek <tkopecek@redhat.com>
2018-03-07 10:15:12 +0100

    fix tests for rpmdiff

--- tests/test_hub/test_rpmdiff.py
+++ tests/test_hub/test_rpmdiff.py
@@ -8,29 +8,29 @@ import kojihub
 
 class TestRPMDiff(unittest.TestCase):
 
-    @mock.patch('kojihub._rpmdiff.Rpmdiff')
+    @mock.patch('koji.rpmdiff.Rpmdiff')
     def test_rpmdiff_empty_invocation(self, Rpmdiff):
-        kojihub.rpmdiff('basepath', [])
+        kojihub.rpmdiff('basepath', [], hashes={})
         Rpmdiff.assert_not_called()
-        kojihub.rpmdiff('basepath', ['foo'])
+        kojihub.rpmdiff('basepath', ['foo'], hashes={})
         Rpmdiff.assert_not_called()
 
-    @mock.patch('kojihub._rpmdiff.Rpmdiff')
+    @mock.patch('koji.rpmdiff.Rpmdiff')
     def test_rpmdiff_simple_success(self, Rpmdiff):
         d = mock.MagicMock()
         d.differs.return_value = False
         Rpmdiff.return_value = d
-        self.assertFalse(kojihub.rpmdiff('basepath', ['foo', 'bar']))
-        Rpmdiff.assert_called_once_with('basepath/foo', 'basepath/bar', ignore='S5TN')
+        self.assertFalse(kojihub.rpmdiff('basepath', ['12/1234/foo', '23/2345/bar'], hashes={}))
+        Rpmdiff.assert_called_once_with('basepath/12/1234/foo', 'basepath/23/2345/bar', ignore='S5TN')
 
-    @mock.patch('kojihub._rpmdiff.Rpmdiff')
+    @mock.patch('koji.rpmdiff.Rpmdiff')
     def test_rpmdiff_simple_failure(self, Rpmdiff):
         d = mock.MagicMock()
         d.differs.return_value = True
         Rpmdiff.return_value = d
         with self.assertRaises(koji.BuildError):
-            kojihub.rpmdiff('basepath', ['foo', 'bar'])
-        Rpmdiff.assert_called_once_with('basepath/foo', 'basepath/bar', ignore='S5TN')
+            kojihub.rpmdiff('basepath', ['12/1234/foo', '13/1345/bar'], hashes={})
+        Rpmdiff.assert_called_once_with('basepath/12/1234/foo', 'basepath/13/1345/bar', ignore='S5TN')
         d.textdiff.assert_called_once_with()
 
 class TestCheckNoarchRpms(unittest.TestCase):
@@ -42,10 +42,10 @@ class TestCheckNoarchRpms(unittest.TestCase):
 
     @mock.patch('kojihub.rpmdiff')
     def test_check_noarch_rpms_simple_invocation(self, rpmdiff):
-        originals = ['foo.noarch.rpm', 'bar.noarch.rpm']
+        originals = ['12/1234/foo.noarch.rpm', '23/2345/foo.noarch.rpm']
         result = kojihub.check_noarch_rpms('basepath', copy.copy(originals))
-        self.assertEquals(result, originals)
-        self.assertEquals(len(rpmdiff.mock_calls), 2)
+        self.assertEquals(result, originals[0:1])
+        self.assertEquals(len(rpmdiff.mock_calls), 1)
 
     @mock.patch('kojihub.rpmdiff')
     def test_check_noarch_rpms_with_duplicates(self, rpmdiff):
@@ -56,7 +56,7 @@ class TestCheckNoarchRpms(unittest.TestCase):
         ]
         result = kojihub.check_noarch_rpms('basepath', copy.copy(originals))
         self.assertEquals(result, ['bar.noarch.rpm'])
-        rpmdiff.assert_called_once_with('basepath', originals)
+        rpmdiff.assert_called_once_with('basepath', originals, hashes={})
 
     @mock.patch('kojihub.rpmdiff')
     def test_check_noarch_rpms_with_mixed(self, rpmdiff):
@@ -70,6 +70,8 @@ class TestCheckNoarchRpms(unittest.TestCase):
         self.assertEquals(result, [
             'foo.x86_64.rpm', 'bar.x86_64.rpm', 'bar.noarch.rpm'
         ])
-        rpmdiff.assert_called_once_with('basepath', [
-            'bar.noarch.rpm', 'bar.noarch.rpm'
-        ])
+        rpmdiff.assert_called_once_with(
+            'basepath',
+            ['bar.noarch.rpm', 'bar.noarch.rpm'],
+            hashes={}
+        )
