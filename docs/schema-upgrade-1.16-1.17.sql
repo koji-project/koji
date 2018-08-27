3d6b640c56e2e0db0021095d4fbaf17b723b0af7
Tomas Kopecek <tkopecek@redhat.com>
2018-08-27 12:06:27 +0200

    Modify SQL update scripts

--- docs/schema-upgrade-1.16-1.17.sql
+++ docs/schema-upgrade-1.16-1.17.sql
@@ -7,4 +7,7 @@ BEGIN;
 -- Change VARCHAR field for build_target names to TEXT to allow longer names
 ALTER TABLE build_target ALTER COLUMN name TYPE TEXT;
 
+-- Allow different merge modes for mergerepo
+ALTER TABLE tag_external_repos ADD COLUMN merge_mode TEXT DEFAULT 'koji';
+
 COMMIT;
