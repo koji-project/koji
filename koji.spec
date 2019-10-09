af66445883c23d40e32bba44b6db517a37c74933
Mike McLean <mikem@redhat.com>
2019-10-09 10:42:17 -0400

    bump version to match most recent release

--- koji.spec
+++ koji.spec
@@ -81,7 +81,7 @@
 %define release %{baserelease}
 %endif
 Name: koji
-Version: 1.18.0
+Version: 1.18.1
 Release: %{release}%{?dist}
 License: LGPLv2 and GPLv2+
 # the included arch lib from yum's rpmUtils is GPLv2+
--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def get_install_requires():
 
 setup(
     name="koji",
-    version="1.18.0",
+    version="1.18.1",
     description=("Koji is a system for building and tracking RPMS. The base"
                  " package contains shared libraries and the command-line"
                  " interface."),
