ba02c1b977f78be73eef2cf234fc233ccaa07e13
Tomas Kopecek <tkopecek@redhat.com>
2020-05-19 11:06:52 +0200

    improve race condition for getNextRelease / images
    Fixes: https://pagure.io/koji/issue/2138

--- hub/kojihub.py
+++ hub/kojihub.py
@@ -13975,6 +13975,8 @@ class HostExports(object):
         data['owner'] = task.getOwner()
         data['state'] = koji.BUILD_STATES['BUILDING']
         data['completion_time'] = None
+        if data.get('release') is None:
+            data['release'] = get_next_release(build_info)
         build_id = new_build(data)
         data['id'] = build_id
         new_image_build(data)
