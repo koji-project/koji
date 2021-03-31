3d33e2e6288d9f062594f7388c51ef2e11ba5293
Jana Cupova <jcupova@redhat.com>
2021-03-31 12:24:48 +0200

    api: getVolume with strict
    Fixes: https://pagure.io/koji/issue/1592

---
+++ tests/test_hub/test_get_volume.py
@@ -0,0 +1,32 @@
+import unittest
+import mock
+
+import koji
+import kojihub
+
+
+class TestGetVolume(unittest.TestCase):
+
+    def setUp(self):
+        self.exports = kojihub.RootExports()
+        self.lookup_name = mock.patch('kojihub.lookup_name').start()
+
+    def test_non_exist_volume_with_strict(self):
+        volume = ['test-volume']
+        self.lookup_name.return_value = None
+        with self.assertRaises(koji.GenericError) as cm:
+            self.exports.getVolume(volume, strict=True)
+        self.assertEqual("No such volume: %s" % volume, str(cm.exception))
+
+    def test_non_exist_volume_with_strict(self):
+        volume = ['test-volume']
+        self.lookup_name.return_value = None
+        result = self.exports.getVolume(volume)
+        self.assertEqual(None, result)
+
+    def test_valid_volume(self):
+        volume = ['test-volume']
+        volume_dict = {'id': 0, 'name': 'DEFAULT'}
+        self.lookup_name.return_value = volume_dict
+        result = self.exports.getVolume(volume)
+        self.assertEqual(volume_dict, result)
