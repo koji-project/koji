669b99f4c5d24951472c21bcdf7797081095c141
Tomas Kopecek <tkopecek@redhat.com>
2022-12-12 10:48:40 +0100

    unify migration scripts
    Related: https://pagure.io/koji/issue/3623

--- docs/schema-update-1.29-1.30.sql
+++ docs/schema-update-1.29-1.30.sql
@@ -4,10 +4,4 @@
 
 BEGIN;
 
--- clean some unused old indices if they still exist
--- https://pagure.io/koji/issue/3160
-DROP INDEX IF EXISTS image_listing_archives;
-DROP INDEX IF EXISTS image_listing_rpms;
-DROP INDEX IF EXISTS imageinfo_listing_rpms;
-
 COMMIT;
