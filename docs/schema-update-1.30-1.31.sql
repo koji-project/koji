20ca0ea942bc2f66fa8ef5a28c90c81b3946d38b
Tomas Kopecek <tkopecek@redhat.com>
2023-01-12 11:02:48 +0100

    PR#3615: fix different PG capabilities in schema
    Merges #3615
    https://pagure.io/koji/pull-request/3615
    
    Fixes: #3594
    https://pagure.io/koji/issue/3594
    schema update issue for 1.30->1.31 update

--- docs/schema-update-1.30-1.31.sql
+++ docs/schema-update-1.30-1.31.sql
@@ -2,6 +2,16 @@
 -- from version 1.30 to 1.31
 
 BEGIN;
-    -- index for default search method for rpms
-    CREATE INDEX rpminfo_filename ON rpminfo((name || '-' || version || '-' || release || '.' || arch || '.rpm')) INCLUDE (id);
+    -- index for default search method for rpms, PG11+ can benefit from new include method
+    DO $$
+       DECLARE version integer;
+       BEGIN
+           SELECT current_setting('server_version_num')::integer INTO version;
+           IF version >= 110000 THEN
+               EXECUTE 'CREATE INDEX rpminfo_filename ON rpminfo((name || ''-'' || version || ''-'' || release || ''.'' || arch || ''.rpm'')) INCLUDE (id);';
+           ELSE
+               EXECUTE 'CREATE INDEX rpminfo_filename ON rpminfo((name || ''-'' || version || ''-'' || release || ''.'' || arch || ''.rpm''));';
+           END IF;
+       END
+    $$;
 COMMIT;
