3e09338db74f09e34493082e7841bc999b302002
Jana Cupova <root@localhost>
2023-03-01 13:21:44 +0100

    Increase CLI uni tests

---
+++ tests/test_cli/test_make_task.py
@@ -0,0 +1,109 @@
+from __future__ import absolute_import
+import koji
+import mock
+from six.moves import StringIO
+
+from koji_cli.commands import handle_make_task
+from . import utils
+
+
+class TestAddNotification(utils.CliTestCase):
+    def setUp(self):
+        self.maxDiff = None
+        self.options = mock.MagicMock()
+        self.session = mock.MagicMock()
+        self.session.getAPIVersion.return_value = koji.API_VERSION
+        self.activate_session_mock = mock.patch('koji_cli.commands.activate_session').start()
+        self.error_format = """Usage: %s make-task [options] <method> [<arg> ...]
+(Specify the --help global option for a list of other help options)
+
+%s: error: {message}
+""" % (self.progname, self.progname)
+        self.watch_tasks_mock = mock.patch('koji_cli.commands.watch_tasks').start()
+        self.watch_tasks_mock.return_value = 0
+        self.task_id = 111
+        self.method = 'test_method'
+        self.channel = 'test-channel'
+        self.priority = 1
+        self.arch = 'test-arch'
+
+    def tearDown(self):
+        mock.patch.stopall()
+
+    def test_make_task_without_args(self):
+        arguments = []
+        self.assert_system_exit(
+            handle_make_task,
+            self.options, self.session, arguments,
+            stdout='',
+            stderr=self.format_error_message('Please specify task method at least'),
+            exit_code=2,
+            activate_session=None)
+        self.activate_session_mock.assert_not_called()
+        self.session.makeTask.assert_not_called()
+        self.watch_tasks_mock.assert_not_called()
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    @mock.patch('koji_cli.commands._running_in_bg', return_value=True)
+    def test_make_task_running_bg_without_watch(self, running_in_bg, stdout):
+        arguments = ['--channel', self.channel, '--priority', self.priority, '--arch', self.arch,
+                     self.method]
+        self.session.makeTask.return_value = self.task_id
+        rv = handle_make_task(self.options, self.session, arguments)
+        actual = stdout.getvalue()
+        expected = "Created task id %i\n" % self.task_id
+        self.assertMultiLineEqual(actual, expected)
+        self.assertEqual(rv, None)
+        self.activate_session_mock.assert_called_once_with(self.session, self.options)
+        self.session.makeTask.assert_called_once_with(
+            arch=self.arch, arglist=[], channel=self.channel, method=self.method,
+            priority=self.priority)
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    @mock.patch('koji_cli.commands._running_in_bg', return_value=False)
+    def test_make_task_not_running_bg_with_watch(self, running_in_bg, stdout):
+        arguments = ['--channel', self.channel, '--priority', self.priority, '--arch', self.arch,
+                     '--watch', self.method]
+        task_id = 111
+        self.session.makeTask.return_value = task_id
+        rv = handle_make_task(self.options, self.session, arguments)
+        actual = stdout.getvalue()
+        expected = "Created task id %i\n" % task_id
+        self.assertMultiLineEqual(actual, expected)
+        self.assertEqual(rv, 0)
+        self.activate_session_mock.assert_called_once_with(self.session, self.options)
+        self.session.makeTask.assert_called_once_with(
+            arch=self.arch, arglist=[], channel=self.channel, method=self.method,
+            priority=self.priority)
+
+    @mock.patch('sys.stdout', new_callable=StringIO)
+    @mock.patch('koji_cli.commands._running_in_bg', return_value=False)
+    def test_make_task_not_running_bg_without_watch(self, running_in_bg, stdout):
+        arguments = ['--channel', self.channel, '--priority', self.priority, '--arch', self.arch,
+                     self.method]
+        task_id = 111
+        self.session.makeTask.return_value = task_id
+        rv = handle_make_task(self.options, self.session, arguments)
+        actual = stdout.getvalue()
+        expected = "Created task id %i\n" % task_id
+        self.assertMultiLineEqual(actual, expected)
+        self.assertEqual(rv, None)
+        self.activate_session_mock.assert_called_once_with(self.session, self.options)
+        self.session.makeTask.assert_called_once_with(
+            arch=self.arch, arglist=[], channel=self.channel, method=self.method,
+            priority=self.priority)
+        self.watch_tasks_mock.assert_not_called()
+
+    def test_handle_make_task_help(self):
+        self.assert_help(
+            handle_make_task,
+            """Usage: %s make-task [options] <method> [<arg> ...]
+(Specify the --help global option for a list of other help options)
+
+Options:
+  -h, --help           show this help message and exit
+  --channel=CHANNEL    set channel
+  --priority=PRIORITY  set priority
+  --watch              watch the task
+  --arch=ARCH          set arch
+""" % self.progname)
--- tests/test_cli/test_win_build.py
+++ tests/test_cli/test_win_build.py
@@ -12,8 +12,13 @@ class TestWinBuild(utils.CliTestCase):
     def setUp(self):
         self.options = mock.MagicMock()
         self.options.debug = False
+        self.options.quiet = None
+        self.options.weburl = 'https://testwebkoji.org'
+        self.options.poll_interval = 0
         self.session = mock.MagicMock()
         self.session.getAPIVersion.return_value = koji.API_VERSION
+        self.stderr = mock.patch('sys.stderr', new_callable=StringIO).start()
+        self.stdout = mock.patch('sys.stdout', new_callable=StringIO).start()
         self.target = 'test-target'
         self.dest_tag = 'destination-test_tag'
         self.target_info = {'build_tag': 4,
@@ -25,57 +30,119 @@ class TestWinBuild(utils.CliTestCase):
         self.scm_url = 'git://test.redhat.com/rpms/pkg-1.1.0' \
                        '?#3fab2ea42ecdc30a41daf1306154dfa04c4d64fd'
         self.vm = 'test-vm'
+        self.watch_tasks_mock = mock.patch('koji_cli.commands.watch_tasks').start()
+        self.watch_tasks_mock.return_value = 0
+        self.running_in_bg_mock = mock.patch('koji_cli.commands._running_in_bg').start()
+        self.error_format = """Usage: %s win-build [options] <target> <URL> <VM>
+(Specify the --help global option for a list of other help options)
 
-    @mock.patch('sys.stderr', new_callable=StringIO)
-    def test_win_build_without_option(self, stderr):
-        expected = "Usage: %s win-build [options] <target> <URL> <VM>\n" \
-                   "(Specify the --help global option for a list of other help options)\n\n" \
-                   "%s: error: Exactly three arguments (a build target, a SCM URL, " \
-                   "and a VM name) are required\n" % (self.progname, self.progname)
-        with self.assertRaises(SystemExit) as ex:
-            handle_win_build(self.options, self.session, [])
-        self.assertExitCode(ex, 2)
-        self.assert_console_message(stderr, expected)
+%s: error: {message}
+""" % (self.progname, self.progname)
 
-    @mock.patch('sys.stderr', new_callable=StringIO)
-    def test_win_build_non_exist_build_target(self, stderr):
-        expected = "Usage: %s win-build [options] <target> <URL> <VM>\n" \
-                   "(Specify the --help global option for a list of other help options)\n\n" \
-                   "%s: error: No such build target: %s\n" % (self.progname, self.progname,
-                                                              self.target)
+    def test_win_build_without_option(self):
+        self.assert_system_exit(
+            handle_win_build,
+            self.options, self.session, [],
+            stderr=self.format_error_message(
+                "Exactly three arguments (a build target, a SCM URL, and a VM name) are required"),
+            stdout='',
+            activate_session=None,
+            exit_code=2)
+        self.session.getBuildTarget.assert_not_called()
+        self.session.getTag.assert_not_called()
+        self.running_in_bg_mock.assert_not_called()
+        self.session.logout.assert_not_called()
+        self.watch_tasks_mock.assert_not_called()
+
+    def test_win_build_non_exist_build_target(self):
         self.session.getBuildTarget.return_value = None
-        with self.assertRaises(SystemExit) as ex:
-            handle_win_build(self.options, self.session, [self.target, self.scm_url, self.vm])
-        self.assertExitCode(ex, 2)
-        self.assert_console_message(stderr, expected)
+        self.assert_system_exit(
+            handle_win_build,
+            self.options, self.session, [self.target, self.scm_url, self.vm],
+            stderr=self.format_error_message("No such build target: %s" % self.target),
+            stdout='',
+            activate_session=None,
+            exit_code=2)
+        self.session.getBuildTarget.assert_called_once_with(self.target)
+        self.session.getTag.assert_not_called()
+        self.running_in_bg_mock.assert_not_called()
+        self.session.logout.assert_not_called()
+        self.watch_tasks_mock.assert_not_called()
 
-    @mock.patch('sys.stderr', new_callable=StringIO)
-    def test_win_build_non_exist_dest_tag(self, stderr):
-        expected = "Usage: %s win-build [options] <target> <URL> <VM>\n" \
-                   "(Specify the --help global option for a list of other help options)\n\n" \
-                   "%s: error: No such destination tag: %s\n" % (self.progname, self.progname,
-                                                                 self.dest_tag)
+    def test_win_build_non_exist_dest_tag(self):
         self.session.getBuildTarget.return_value = self.target_info
         self.session.getTag.return_value = None
-        with self.assertRaises(SystemExit) as ex:
-            handle_win_build(self.options, self.session, [self.target, self.scm_url, self.vm])
-        self.assertExitCode(ex, 2)
-        self.assert_console_message(stderr, expected)
+        self.assert_system_exit(
+            handle_win_build,
+            self.options, self.session, [self.target, self.scm_url, self.vm],
+            stderr=self.format_error_message("No such destination tag: %s" % self.dest_tag),
+            stdout='',
+            activate_session=None,
+            exit_code=2)
+        self.session.getBuildTarget.assert_called_once_with(self.target)
+        self.session.getTag.assert_called_once_with(self.target_info['dest_tag'])
+        self.running_in_bg_mock.assert_not_called()
+        self.session.logout.assert_not_called()
+        self.watch_tasks_mock.assert_not_called()
 
-    @mock.patch('sys.stderr', new_callable=StringIO)
-    def test_win_build_dest_tag_locked(self, stderr):
-        expected = "Usage: %s win-build [options] <target> <URL> <VM>\n" \
-                   "(Specify the --help global option for a list of other help options)\n\n" \
-                   "%s: error: Destination tag %s is locked\n" % (self.progname, self.progname,
-                                                                  self.dest_tag)
+    def test_win_build_dest_tag_locked(self):
         dest_tag_info = {'name': self.dest_tag, 'locked': True}
 
         self.session.getBuildTarget.return_value = self.target_info
         self.session.getTag.return_value = dest_tag_info
-        with self.assertRaises(SystemExit) as ex:
-            handle_win_build(self.options, self.session, [self.target, self.scm_url, self.vm])
-        self.assertExitCode(ex, 2)
-        self.assert_console_message(stderr, expected)
+        self.assert_system_exit(
+            handle_win_build,
+            self.options, self.session, [self.target, self.scm_url, self.vm],
+            stderr=self.format_error_message("Destination tag %s is locked" % self.dest_tag),
+            stdout='',
+            activate_session=None,
+            exit_code=2)
+        self.session.getBuildTarget.assert_called_once_with(self.target)
+        self.session.getTag.assert_called_once_with(self.target_info['dest_tag'])
+        self.running_in_bg_mock.assert_not_called()
+        self.session.logout.assert_not_called()
+        self.watch_tasks_mock.assert_not_called()
+
+    def test_win_build_not_queit_and_repo_id(self):
+        self.running_in_bg_mock.return_value = True
+        task_id = 111
+        expected_output = """Created task: %d
+Task info: %s/taskinfo?taskID=%s
+""" % (task_id, self.options.weburl, task_id)
+        self.session.winBuild.return_value = task_id
+        rv = handle_win_build(self.options, self.session,
+                              ["none", self.scm_url, self.vm, '--background', '--repo-id=10'])
+        self.assertEqual(rv, None)
+        self.assert_console_message(self.stdout, expected_output)
+        self.assert_console_message(self.stderr, "")
+        self.session.getBuildTarget.assert_not_called()
+        self.session.getTag.assert_not_called()
+        self.running_in_bg_mock.assert_called_once()
+        self.session.logout.assert_not_called()
+        self.watch_tasks_mock.assert_not_called()
+
+    def test_win_build_wait_opt(self):
+        task_id = 111
+        dest_tag_info = {'name': self.dest_tag, 'locked': False}
+
+        self.session.getBuildTarget.return_value = self.target_info
+        self.session.getTag.return_value = dest_tag_info
+        expected_output = """Created task: %d
+Task info: %s/taskinfo?taskID=%s
+""" % (task_id, self.options.weburl, task_id)
+        self.session.winBuild.return_value = task_id
+        rv = handle_win_build(self.options, self.session,
+                              [self.target, self.scm_url, self.vm, '--wait'])
+        self.assertEqual(rv, 0)
+        self.assert_console_message(self.stdout, expected_output)
+        self.assert_console_message(self.stderr, "")
+        self.session.getBuildTarget.assert_called_once_with(self.target)
+        self.session.getTag.assert_called_once_with(self.target_info['dest_tag'])
+        self.running_in_bg_mock.assert_not_called()
+        self.session.logout.assert_called_once()
+        self.watch_tasks_mock.assert_called_once_with(
+            self.session, [task_id], quiet=self.options.quiet,
+            poll_interval=self.options.poll_interval, topurl=self.options.topurl)
 
     def test_win_build_help(self):
         self.assert_help(
