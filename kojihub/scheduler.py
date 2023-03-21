cc33d2b1244586feced27fa2d26fa5c26e202fba
Mike McLean <mikem@redhat.com>
2023-03-20 22:31:12 -0400

    more work

--- kojihub/scheduler.py
+++ kojihub/scheduler.py
@@ -76,10 +76,15 @@ class TaskScheduler(object):
             runs_by_task[run['task_id']].append(run)
 
         # get tasks to schedule
-        tasks = self.get_tasks()
+        tasks = self.get_free_tasks()
+        tasks_by_bin = {}
+        for task in tasks:
+            tbin = '%(channel_id)s:%(arch)s' % task
+            task['_bin'] = tbin
+            tasks_by_bin.setdefault(tbin, []).append(task)
 
         # get hosts and bin them
-        hosts = get_ready_hosts()
+        hosts = self.get_ready_hosts()
         hosts_by_bin = {}
         for host in hosts:
             host['_bins'] = []
@@ -89,10 +94,34 @@ class TaskScheduler(object):
                     hosts_by_bin.setdefault(host_bin, []).append(host)
                     host['_bins'].append(host_bin)
 
-        for task in active_tasks:
-            if task['state'] == koji.TASK_STATES['ASSIGNED']:
-                # TODO -- sort out our interaction with old school assignments
-                continue
+        # order bins by available host capacity
+        order = []
+        for _bin in hosts_by_bin:
+            hosts = hosts_by_bin[_bin]
+            avail = sum([min(0, h['capacity'] - h['task_load']) for h in hosts])
+            order.append((avail, _bin))
+        order.sort()
+
+        # note bin demand for each host
+        for n, (avail, _bin) in enumerate(order):
+            rank = float(n) / len(order)
+            for host in hosts_by_bin[_bin]:
+                host.setdefault('_rank', rank)
+                # so host rank is set by the most contentious bin it covers
+                # TODO - we could be smarter here, but it's a start
+
+        # sort binned hosts by rank
+        for _bin in hosts_by_bin:
+            hosts = hosts_by_bin[_bin]
+            hosts.sort(key=lambda h: h._rank, reverse=True)
+            # hosts with least contention first
+
+        # tasks are already in priority order
+        for task in tasks:
+            hosts = hosts_by_bin[task['_bin']]
+            # these are the hosts that _can_ take this task
+            # TODO - update host ranks as we go
+
             have_run = False
             task_runs = runs_by_task.get(task['id'], [])
             for run in task_runs:
@@ -112,7 +141,7 @@ class TaskScheduler(object):
         # indicate that scheduling ran
         return True
 
-    def get_tasks(self):
+    def get_free_tasks(self):
         """Get the tasks that need scheduling"""
 
         fields = (
@@ -124,22 +153,70 @@ class TaskScheduler(object):
             ('method', 'method'),
             ('priority', 'priority'),
             ("date_part('epoch', task.create_time)", 'create_ts'),
-            ('scheduler_task_runs.id', 'run_id'),
+            # ('scheduler_task_runs.id', 'run_id'),
         )
         fields, aliases = zip(*fields)
 
-        values = {'states': [koji.TASK_STATES[n] for n in ('FREE', 'ASSIGNED')]}
+        values = {'states': [koji.TASK_STATES[n] for n in ('FREE',)]}
 
         query = QueryProcessor(
             columns=fields, aliases=aliases, tables=['task'],
-            joins=('LEFT OUTER JOIN scheduler_task_runs ON task_id = task.id'),
-            clauses=('task.state IN %(states)s', 'run_id IS NULL'),
-            # XXX these clauses are not enough
+            # joins=('LEFT OUTER JOIN scheduler_task_runs ON task_id = task.id'),
+            # clauses=('task.state IN %(states)s', 'run_id IS NULL'),
+            clauses=('task.state IN %(states)s',),
             values=values,
-            opts={'order': 'priority,create_time'}
+            opts={'order': 'priority,create_time'},
+            # scheduler order
+            # lower priority numbers take precedence, like posix process priority
+            # at a given priority, earlier creation times take precedence
         )
 
         return query.execute()
+
+    def get_ready_hosts():
+        """Query hosts that are ready to build"""
+
+        fields = (
+            ('host.id', 'id'),
+            ('host.name', 'name'),
+            ('host.task_load', 'task_load'),
+            ('host_config.arches', 'arches'),
+            ('host_config.capacity', 'capacity'),
+            ("date_part('epoch', sessions.update_time)", 'update_ts'),
+        )
+        fields, aliases = zip(*fields)
+
+        query = QueryProcessor(
+            tables=['host'],
+            columns=fields,
+            aliases=aliases,
+            clauses=[
+                'enabled IS TRUE',
+                'ready IS TRUE',
+                'expired IS FALSE',
+                'master IS NULL',
+                'active IS TRUE',
+                "update_time > NOW() - '5 minutes'::interval"
+            ],
+            joins=[
+                'sessions USING (user_id)',
+                'host_config ON host.id = host_config.host_id'
+            ]
+        )
+
+        hosts = query.execute()
+        for host in hosts:
+            query = QueryProcessor(
+                tables=['host_channels'],
+                columns=['channel_id'],
+                clauses=['host_id=%(id)s', 'active IS TRUE', 'enabled IS TRUE'],
+                joins=['channels ON host_channels.channel_id = channels.id'],
+                values=host
+            )
+            rows = query.execute()
+            host['channels'] = [row['channel_id'] for row in rows]
+
+        return hosts
 
     def add_run(self, task, host):
         insert = InsertProcessor('scheduler_runs')
