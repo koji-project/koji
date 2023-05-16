b9e9dd6c4b6ce949bda86b1dbc01537dadb4d601
Tomas Kopecek <tkopecek@redhat.com>
2023-05-16 13:17:04 +0200

    migration script

--- docs/schema-upgrade-1.32-1.33.sql
+++ docs/schema-upgrade-1.32-1.33.sql
@@ -8,4 +8,8 @@ BEGIN;
     INSERT INTO archivetypes (name, description, extensions) VALUES ('packages', 'Kiwi packages listing', 'packages') ON CONFLICT DO NOTHING;
     INSERT INTO archivetypes (name, description, extensions) VALUES ('verified', 'Kiwi verified package list', 'verified') ON CONFLICT DO NOTHING;
     ALTER TABLE host ADD COLUMN update_time TIMESTAMPTZ;
+    CREATE TABLE locks (
+        name TEXT NOT NULL PRIMARY KEY
+    ) WITHOUT OIDS;
+    INSERT INTO locks(name) VALUES('protonmsg-plugin');
 COMMIT;
