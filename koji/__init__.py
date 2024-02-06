598e8c316704621b0374d303f6f38f133bc650e4
Tomas Kopecek <tkopecek@redhat.com>
2024-02-06 11:10:53 +0100

    PR#4005: Fix bandit "nosec" comments
    Merges #4005
    https://pagure.io/koji/pull-request/4005
    
    Fixes: #4004
    https://pagure.io/koji/issue/4004
    Fix bandit warnings

--- koji/__init__.py
+++ koji/__init__.py
@@ -1503,13 +1503,15 @@ def parse_pom(path=None, contents=None):
     contents = fixEncoding(contents)
 
     try:
-        xml.sax.parseString(contents, handler)  # nosec - trusted data
+        # trusted data, skipping bandit test
+        xml.sax.parseString(contents, handler)  # nosec
     except xml.sax.SAXParseException:
         # likely an undefined entity reference, so lets try replacing
         # any entity refs we can find and see if we get something parseable
         handler.reset()
         contents = ENTITY_RE.sub('?', contents)
-        xml.sax.parseString(contents, handler)  # nosec - trusted data
+        # trusted data, skipping bandit test
+        xml.sax.parseString(contents, handler)  # nosec
 
     for field in fields:
         if field not in util.to_list(values.keys()):
