5bdcb2a3a5d01d89ae383fe1e7f487530c85207d
root <root@mvf-alma9.cern.ch>
2025-02-19 15:24:33 +0100

    use unittest.mock instead of mock

--- tests/test_kojira/test_main.py
+++ tests/test_kojira/test_main.py
@@ -7,7 +7,10 @@ import tempfile
 import time
 import unittest
 
-import mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 import pytest
 
 import koji
--- tests/test_plugins/test_builder_scmpolicy.py
+++ tests/test_plugins/test_builder_scmpolicy.py
@@ -1,5 +1,8 @@
 import unittest
-import mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 
 import scmpolicy
 import koji
